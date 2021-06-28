4d646a1f1a1a95cb2198b1609816c8b303930c12
fabriciored <fabriciojlsantos@gmail.com>
2021-06-28 15:41:08 -0300

    feat: admin controls

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,6 +1,6 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
-import '../styles/question.scss';
+import "../styles/question.scss";
 
 type QuestionProps = {
   content: string;
@@ -9,20 +9,30 @@ type QuestionProps = {
     avatar: string;
   };
   children?: ReactNode;
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
 };
 
-export function Question({ content, author, children }: QuestionProps) {
+export function Question({
+  content,
+  author,
+  children,
+  isAnswered = false,
+  isHighlighted = false,
+}: QuestionProps) {
   return (
-    <div className="question">
+    <div
+      className={`question ${isAnswered ? "answered" : ""} ${
+        isHighlighted && !isAnswered ? "highlighted" : ""
+      }`}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
         </div>
-        <div>
-          {children}
-        </div>
+        <div>{children}</div>
       </footer>
     </div>
   );
